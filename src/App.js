10cbeb94cd34ed2f7ad7b3b12416f9a6d95d68e8
Bradley <bradley.t.sheehan@gmail.com>
2023-03-13 16:41:03 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -236,7 +236,7 @@ function App() {
                 textAlign: "center",
                 color: "var(--primary-text)",
               }}
-            >
+            > 
               <StyledLink target={"_blank"} href={CONFIG.SCAN_LINK}>
                 {truncate(CONFIG.CONTRACT_ADDRESS, 15)}
               </StyledLink>
